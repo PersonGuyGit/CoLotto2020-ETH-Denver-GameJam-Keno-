1080e860591995cd0bb9205c882c0ddc0bba1f29
Luke Gebbink <lukegebbink@gmail.com>
2020-08-01 09:03:19 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CoLottoKeno
+
+Developed with Unreal Engine 4
---
+++ Source/CoLottoKeno/CoLottoKenoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CoLottoKenoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COLOTTOKENO_API ACoLottoKenoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
